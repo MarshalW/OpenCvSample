a4c3ef35a2131638f450c578414c745ada4f7353
Marshal Wu <marshal.wu@gmail.com>
2013-07-22 17:39:58 +0800

    最简单的摄像头显示视频

---
+++ src/com/example/OpenCvSample/MyActivity.java
@@ -0,0 +1,47 @@
+package com.example.OpenCvSample;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import android.view.WindowManager;
+import org.opencv.android.*;
+
+public class MyActivity extends Activity {
+
+    private JavaCameraView cameraView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        setContentView(R.layout.main);
+        cameraView = (JavaCameraView) findViewById(R.id.camView);
+    }
+
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS:
+                    cameraView.enableView();
+                    break;
+                default:
+                    super.onManagerConnected(status);
+            }
+        }
+    };
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (cameraView != null) {
+            cameraView.disableView();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_6, this, mLoaderCallback);
+    }
+}
